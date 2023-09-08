21a894d16976ac192a28e2092eeb9f1ce42225a3
Zhang Ji Peng <onecoolx@gmail.com>
2023-09-08 17:18:27 +0800

    Update build_linux.sh
    ignore make error for GNU Make4

--- build_linux.sh
+++ build_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 ./tools/gyp/gyp --depth=./ picasso.gyp --generator-output=proj
-echo "Please change dir to \"proj\" and type \"make\" or \"make BUILDTYPE=Release\""
+echo "Please change dir to \"proj\" and type \"make -i\" or \"make BUILDTYPE=Release -i\""
 echo "...\n"		
