8492a3d760bd8e8204a649345cca287b4345830c
zhangjipeng <zhangjipeng@xiaomi.com>
2024-07-03 19:33:08 +0800

    change default windows build for cmake

--- build_windows.bat
+++ build_windows.bat
@@ -1,3 +1,6 @@
-set GYP_MSVS_VERSION=2019
-./tools/gyp/gyp --depth=./ picasso.gyp --generator-output=vcproj
+rem set GYP_MSVS_VERSION=2019
+rem ./tools/gyp/gyp --depth=./ picasso.gyp --generator-output=vcproj
 
+mkdir proj
+cd proj
+cmake ../
