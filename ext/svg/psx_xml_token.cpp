37f1679e6cd93de2ac3f5260eb8ede7ad766cc4f
zhangjipeng <zhangjipeng@xiaomi.com>
2025-02-12 20:08:27 +0800

    add enum defind for xml tokenizer

--- ext/svg/psx_xml_token.cpp
+++ ext/svg/psx_xml_token.cpp
@@ -35,6 +35,56 @@ extern "C" {
 
 #define TOKEN_LEN(t) ((t)->end - (t)->start)
 
+/*
+ *   tag mask   entity mask  quote mask   tag   search  comment  doctype   xmlinst   server side   script
+ * |  0 0 0   |   0 0 0    |    0 0     |  0  |   0   |    0   |    0    |    0    |      0     |    0    |
+ */
+enum {
+    IN_TAG_MASK = (1 << 3) - 1, // we need 3 bits to store the state of tag.
+    IN_ENTITY_MASK = (1 << 6) - (1 << 3), //we need 3 bits to store the state of entity.
+    IN_QUOTE_MASK = (1 << 8) - (1 << 6), //we need 2 bit to store quote state.
+    IN_START_TAG = 1 << 8,
+    IN_SEARCH = 1 << 9,
+    IN_COMMENT = 1 << 10,
+    IN_DOCTYPE = 1 << 11,
+    IN_XMLINST = 1 << 12,
+    IN_SERVER_SIDE = 1 << 13,
+    IN_SCRIPT = 1 << 14,
+};
+
+enum {
+    NO_QUOTE = 0,
+    SINGLE_QUOTE = 1,
+    SDOUBLE_QUOTE = 2,
+};
+
+enum {
+    NO_ENTITY = 0,
+    START_ENTITY = 1,
+    NUMERIC = 2,
+    HEX_DECIMAL = 3,
+    DECIMAL = 4,
+    ENTITY_NAME = 5,
+    SEMICOLON = 6,
+};
+
+enum {
+    NO_TAG = 0,
+    TAG_NAME = 1,
+    ATTR_START = 2,
+    ATTR_NAME = 3,
+    SEARCH_EQUAL = 4,
+    SEARCH_VALUE = 5,
+    QUOTE_VALUE = 6,
+    VALUE = 7,
+};
+
+typedef struct {
+    uint32_t flags;
+    const char* cur;
+    const char* end;
+} xml_token_state_t;
+
 bool psx_xml_tokenizer(const char* xml_data, uint32_t data_len, xml_token_process, void* data)
 {
     return false;
