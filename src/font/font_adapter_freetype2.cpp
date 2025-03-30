4cacab8e8c3f52dfdb8efd7d93ba81eff2ca196d
Benson Muite <bkmgit@users.noreply.github.com>
2025-03-30 15:08:10 +0300

    Ensure builds without --fpermissive flag

--- src/font/font_adapter_freetype2.cpp
+++ src/font/font_adapter_freetype2.cpp
@@ -198,7 +198,8 @@ static bool decompose_ft_outline(const FT_Outline& outline,
         v_control = v_start;
 
         point = outline.points + first;
-        tags = outline.tags + first;
+        tags = outline.tags;
+        tags += first;
         tag = FT_CURVE_TAG(tags[0]);
 
         // A contour cannot start with a cubic control point!
