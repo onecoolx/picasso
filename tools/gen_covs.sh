92ad1d7e4877c0a386faaa7aef1dbc85bc871491
zhangjipeng <zhangjipeng@xiaomi.com>
2025-12-29 16:57:34 +0800

    fix coverage gen script

--- tools/gen_covs.sh
+++ tools/gen_covs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 lcov -d . -c -o picasso.info
-lcov --remove picasso.info -o picasso.info '/usr/*' '*/proj/*' '*/unit_tests/*'
+lcov --remove picasso.info -o picasso.info '/usr/*' '*/proj/*' '*/unit_tests/*' '*/third_party/*'
 genhtml -o result picasso.info
