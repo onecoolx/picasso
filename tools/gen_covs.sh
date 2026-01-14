fd48d0a4f1110fa149d5364fe04578724f7f17c2
zhangjipeng <zhangjipeng@xiaomi.com>
2026-01-14 20:12:40 +0800

    chore(fix): fix conditional syntax for coverage generation workflow
    Signed-off-by: zhangjipeng <zhangjipeng@xiaomi.com>

--- tools/gen_covs.sh
+++ tools/gen_covs.sh
@@ -3,6 +3,6 @@
 GCC_VERSION=`gcc --version | head -n1 | awk '{print $3}'`
 GCC_MAJOR_VERSION=$(echo $GCC_VERSION | cut -d. -f1,1)
 
-lcov --gcov-tool /usr/bin/gcov-${GCC_MAJOR_VERSION} --ignore-errors -d . -c -o picasso.info
+lcov --gcov-tool /usr/bin/gcov-${GCC_MAJOR_VERSION} -d . -c -o picasso.info
 lcov --remove picasso.info -o picasso.info '/usr/*' '*/proj/*' '*/unit_tests/*' '*/third_party/*'
-genhtml -o result picasso.info
+genhtml --ignore-errors -o result picasso.info
