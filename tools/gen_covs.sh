1b689a681152bef6334c1ad14878c455923a9fa3
Zhang Ji Peng <onecoolx@gmail.com>
2026-01-14 20:25:28 +0800

    chore(fix): modify coverage generation script to ignore errors
    Updated lcov and genhtml commands to ignore errors.

--- tools/gen_covs.sh
+++ tools/gen_covs.sh
@@ -3,6 +3,6 @@
 GCC_VERSION=`gcc --version | head -n1 | awk '{print $3}'`
 GCC_MAJOR_VERSION=$(echo $GCC_VERSION | cut -d. -f1,1)
 
-lcov --gcov-tool /usr/bin/gcov-${GCC_MAJOR_VERSION} -d . -c -o picasso.info
+lcov --ignore-errors mismatch --gcov-tool /usr/bin/gcov-${GCC_MAJOR_VERSION} -d . -c -o picasso.info
 lcov --remove picasso.info -o picasso.info '/usr/*' '*/proj/*' '*/unit_tests/*' '*/third_party/*'
-genhtml --ignore-errors -o result picasso.info
+genhtml -o result picasso.info
