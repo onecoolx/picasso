e58bd5aaf6e4f3c3b3bb163f24f6b0d75bf5a678
Zhang Ji Peng <onecoolx@gmail.com>
2026-01-14 20:44:01 +0800

    chore(fix): update lcov command to include unexecuted blocks option

--- tools/gen_covs.sh
+++ tools/gen_covs.sh
@@ -3,6 +3,6 @@
 GCC_VERSION=`gcc --version | head -n1 | awk '{print $3}'`
 GCC_MAJOR_VERSION=$(echo $GCC_VERSION | cut -d. -f1,1)
 
-lcov --ignore-errors mismatch --gcov-tool /usr/bin/gcov-${GCC_MAJOR_VERSION} -d . -c -o picasso.info
+lcov --rc geninfo_unexecuted_blocks=1 --gcov-tool /usr/bin/gcov-${GCC_MAJOR_VERSION} -d . -c -o picasso.info
 lcov --remove picasso.info -o picasso.info '/usr/*' '*/proj/*' '*/unit_tests/*' '*/third_party/*'
 genhtml -o result picasso.info
