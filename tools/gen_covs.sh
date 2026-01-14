92c368e06e9d67bff6bd168fe5a5d1fd3256b0aa
zhangjipeng <zhangjipeng@xiaomi.com>
2026-01-14 18:05:20 +0800

    chore(fix): fix code coverage generate error
    Signed-off-by: zhangjipeng <zhangjipeng@xiaomi.com>

--- tools/gen_covs.sh
+++ tools/gen_covs.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-lcov -d . -c -o picasso.info
+GCC_VERSION=`gcc --version | head -n1 | awk '{print $3}'`
+GCC_MAJOR_VERSION=$(echo $GCC_VERSION | cut -d. -f1,1)
+
+lcov --gcov-tool /usr/bin/gcov-${GCC_MAJOR_VERSION} -d . -c -o picasso.info
 lcov --remove picasso.info -o picasso.info '/usr/*' '*/proj/*' '*/unit_tests/*' '*/third_party/*'
 genhtml -o result picasso.info
